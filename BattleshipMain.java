7d9ee49948e4103af52df08a24bab1e8d3b691a7
okaycomputed <100307637+okaycomputed@users.noreply.github.com>
2024-10-29 17:49:00 +0800

    main program editing

--- BattleshipMain.java
+++ BattleshipMain.java
@@ -21,8 +21,22 @@ public class BattleshipMain {
         d.ShowSelfGrid(bs.GetCurrPlayer().GetSelfGrid());
         d.ShowOpponentGrid(bs.GetCurrPlayer().GetOpponentGrid());
 
-        System.out.println("Please enter the x-coordinate to hit: ");
-        System.out.println("Please enter the y-coordinate to hit: ");
+        System.out.println("Select a ship to carry out the attack: ");
+        System.out.println("1. Carrier");
+        System.out.println("2. Battleship");
+        System.out.println("3. Submarine");
+        System.out.println("4. Patrol Boat");
+        int shipOption = input.nextInt();
 
+        // Insert method to set attacking ship, if ship has already been sunken or if the option is invalid,
+        // return the corresponding error messages
+
+        System.out.print("Please enter the x-coordinate to hit: ");
+        int attackXCor = input.nextInt();
+        System.out.print("Please enter the y-coordinate to hit: ");
+        int attackYCor = input.nextInt();
+
+        // Insert error checking here to ensure that the ship can be hit
+        // return the results of the attack
     }
 }
