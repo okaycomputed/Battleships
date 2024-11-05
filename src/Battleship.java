8ba498d85098cdddadb91837bf8eafecf322fe59
kazazaka <cazival@gmail.com>
2024-11-05 22:17:25 +0800

    wrote the overriding Attack() methods for the Ship subclasses

--- src/Battleship.java
+++ src/Battleship.java
@@ -7,16 +7,17 @@ public class Battleship extends Ship {
 
     public char[][] Attack(int xCor, int yCor, int orientation) {
         char[][] attack = new char[3][2];
-        if (orientation == Player.HORIZONTAL) {
             for (int i = 0; i < attack.length; i++) {
                 int j = 0;
                 attack[i][j] = (char) xCor;
                 attack[i][j+1] = (char) yCor;
-                xCor++;
+                if ( orientation == Player.HORIZONTAL && xCor < BattleshipSystem.GRID_WIDTH - 1) {
+                    xCor++;
+                }
+                else if (orientation == Player.VERTICAL && yCor < BattleshipSystem.GRID_HEIGHT - 1) {
+                    yCor++;
+                }
             }
-        }
-        else if (orientation == Player.VERTICAL) {
-        }
         return attack;
     }
 }
--- src/Carrier.java
+++ src/Carrier.java
@@ -7,6 +7,18 @@ public class Carrier extends Ship {
 
     public char[][] Attack(int xCor, int yCor, int orientation) {
         char[][] attack = new char[9][2];
+
+        int i = 0;
+        for (int m = yCor; m > yCor - 3; m--) {
+                for (int k = xCor; k < xCor + 3; k++) {
+                    if (k < BattleshipSystem.GRID_WIDTH  && m > 0) {
+                            int j = 0;
+                            attack[i][j] = (char) k;
+                            attack[i][j + 1] = (char) m;
+                            i++;
+                    }
+                }
+            }
         return attack;
     }
 }
